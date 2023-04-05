983cd2b2d15157dc9baf6be877f27c492d7b7536
Kennedy Kiarie Gitau <gitaukennedykiarie@gmail.com>
2023-04-05 14:58:37 +0300

    adding local bootstrap css and js

--- content.js
+++ content.js
@@ -21,35 +21,36 @@ function waitUntilVisible(selector, timeout = 5000) {
   });
 }
 
-// Read settings from JSON file
-fetch(chrome.runtime.getURL('settings.json'))
-  .then(response => response.json())
-  .then(settings => {
-    // Wait for the text area element to become visible
-    waitUntilVisible(settings.selector, settings.timeout).then(textArea => {
-      // Wait for an additional 5 seconds before adding input to the textbox
-      setTimeout(() => {
-        // Set the input value
-        textArea.value = settings.inputValue;
-        // Create and trigger a "keydown" event to simulate pressing the "Enter" key
-        const enterEvent = new KeyboardEvent("keydown", {
-          key: "Enter",
-          code: "Enter",
-          which: 13,
-          keyCode: 13,
-          bubbles: true,
-          cancelable: true,
-        });
-        textArea.dispatchEvent(enterEvent);
+// Load the settings from the extension's manifest file
+chrome.runtime.getManifest().then(manifest => {
+  const settings = manifest.chatgpt_jailbreak;
+  
+  // Wait for the text area element to become visible
+  waitUntilVisible(settings.selector, settings.timeout).then(textArea => {
+    // Wait for an additional 5 seconds before adding input to the textbox
+    setTimeout(() => {
+      // Set the input value
+      textArea.value = settings.inputValue;
+      
+      // Create and trigger a "keydown" event to simulate pressing the "Enter" key
+      const enterEvent = new KeyboardEvent("keydown", {
+        key: "Enter",
+        code: "Enter",
+        which: 13,
+        keyCode: 13,
+        bubbles: true,
+        cancelable: true,
+      });
+      textArea.dispatchEvent(enterEvent);
 
-        // Clear the textarea's value
-        textArea.value = "";
+      // Clear the textarea's value
+      textArea.value = "";
 
-        // Trigger the "input" event to notify OpenAI that the input has changed
-        const inputEvent = new Event('input', { bubbles: true });
-        textArea.dispatchEvent(inputEvent);
-      }, settings.delay);
-    }).catch(error => {
-      console.log(error);
-    });
+      // Trigger the "input" event to notify OpenAI that the input has changed
+      const inputEvent = new Event('input', { bubbles: true });
+      textArea.dispatchEvent(inputEvent);
+    }, settings.delay);
+  }).catch(error => {
+    console.log(error);
   });
+});
